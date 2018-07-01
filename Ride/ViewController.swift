1ce853868666a9926e700919fa32bdd6ec8761ee
Keaton Burleson <keaton.burleson@me.com>
2018-06-30 17:24:54 -0700

    Initial Commit

---
+++ Ride/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ride
+//
+//  Created by Keaton Burleson on 6/30/18.
+//  Copyright © 2018 Keaton Burleson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
